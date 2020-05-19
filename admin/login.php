2fb06a94a049e05fead1fc1e354726f14b4b6dac
Android-HelloWorld <mongtamquoc2015@gmail.com>
2020-05-19 21:02:21 +0700

    ket noi database

--- admin/login.php
+++ admin/login.php
@@ -1,3 +1,15 @@
+<?php
+include('../classes/adminlogin.php');
+?>
+<?php
+	$adminLogin = new AdminLogin();
+	if ($_SERVER['REQUEST_METHOD'] === "POST"){
+		$adminUser = $_SERVER['adminUser'];
+		$adminPass = $_SERVER['adminPass'];
+
+		$loginCheck = $adminLogin->login()
+	}
+?>
 <!DOCTYPE html>
 <head>
 <meta charset="utf-8">
@@ -7,13 +19,13 @@
 <body>
 <div class="container">
 	<section id="content">
-		<form action="" method="post">
+		<form action="login.php" method="post">
 			<h1>Admin Login</h1>
 			<div>
-				<input type="text" placeholder="Username" required="" name="username"/>
+				<input type="text" placeholder="Username" required="" name="adminUser"/>
 			</div>
 			<div>
-				<input type="password" placeholder="Password" required="" name="password"/>
+				<input type="password" placeholder="Password" required="" name="adminPass"/>
 			</div>
 			<div>
 				<input type="submit" value="Log in" />
---
+++ classes/AdminLogin.php
@@ -0,0 +1,14 @@
+<?php
+class AdminLogin 
+{
+    public function __construct()
+    {
+        # code...
+    }
+
+    public function login()
+    {
+        
+    }
+}
+?>
