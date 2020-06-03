cd44d6f42cf4c2e523e6f6cf98064794443a3373
Android-HelloWorld <mongtamquoc2015@gmail.com>
2020-06-03 18:08:46 +0700

    Custome footer

--- include/footer.php
+++ include/footer.php
@@ -3,7 +3,7 @@
     <div class="container-fluid pt-5 bg-dark text-light">
         <div class="container">
             <div class="row">
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <div class="row">
                         <h5>Meta</h5>
                     </div>
@@ -18,33 +18,9 @@
                 </div>
 
 
-                <div class="col-md-3">
-                    <div class="row">
-                        <h5>Recent Posts</h5>
-                    </div>
-                    <div class="row mb-4">
-                        <div class="underline bg-light" style="width: 50px"></div>
-                    </div>
-                    <div class="row">
-                        <div class="media">
-                            <img src="img/b1.jpg" class="img-fluid" alt="media-image">
-                            <div class="media-body ml-2">
-                                <h6>ackets For The Soul. What Color Is Yours?</h6>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="media">
-                            <img src="img/b2.jpg" class="img-fluid" alt="media-image">
-                            <div class="media-body ml-2">
-                                <h6>Best Fabrics For Your Dream Dress!</h6>
-                            </div>
-                        </div>
-                    </div>
-                </div>
 
 
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <div class="row">
                         <h5>About</h5>
                     </div>
@@ -56,14 +32,18 @@
                     </div>
                 </div>
 
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <div class="row">
                         <h5>Tags</h5>
                     </div>
                     <div class="row mb-4">
                         <div class="underline bg-light" style="width: 50px"></div>
                     </div>
-                    <button class="btn btn-outline-light">autunm</button> <button class="btn btn-outline-light">Dress</button> <button class="btn btn-outline-light">ladice</button> <button class="btn btn-outline-light">black dressunm</button> <button class="btn btn-outline-light">modern dress</button>
+                    <button class="btn btn-outline-light">autunm</button>
+                    <button class="btn btn-outline-light">Dress</button>
+                    <button class="btn btn-outline-light">ladice</button>
+                    <button class="btn btn-outline-light">black dressunm</button>
+                    <button class="btn btn-outline-light">modern dress</button>
                 </div>
             </div>
         </div>
