1d1e54af6b0ed4283c1cf12793f994f51e06b067
Android-HelloWorld <mongtamquoc2015@gmail.com>
2020-06-08 21:38:25 +0700

    Tao chuc nang gui gmail den cho nguoi dung dang ky thanh cong

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -2,6 +2,12 @@
 $path = realpath(__DIR__);
 include_once $path . '/../models/Session.php';
 include_once $path.'/../models/Database.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+require 'vendor/autoload.php';
+
 ?>
 
 <?php
@@ -31,7 +37,8 @@ class UserController
             $query = "INSERT INTO tbl_user(email,password,name) VALUES('$email','$password','$name')";
             $result = $this->database->insert($query);
             if ($result) {
-                echo '<script>window.location.replace("login.php")</script>';
+                $this->sendMail($email,$name);
+                 echo '<script>window.location.replace("login.php")</script>';
             }
         } else {
             return '<div class="alert alert-danger">Field must no empty!</div>';
@@ -40,7 +47,6 @@ class UserController
 
     public function login($email, $password)
     {
-
         $email = mysqli_real_escape_string($this->database->link, $email);
         $password = mysqli_real_escape_string($this->database->link, $password);
 
@@ -54,10 +60,49 @@ class UserController
                 Session::set('email', $row['email']);
                 Session::set('name', $row['name']);
                 Session::set('id', $row['id']);
+
                 echo '<script>window.location.replace("index.php")</script>';
             } else {
                 return '<div class="alert alert-danger">Password does not match</div>';
             }
         }
     }
+
+    public function sendMail($to,$name)
+    {
+        $query = "SELECT * FROM tbl_user WHERE email = '$to' AND name = '$name'";
+        $resultSelect = $this->database->select($query);
+        if ($resultSelect) {
+            try {
+                //Server settings
+                $mail = new PHPMailer();
+                $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+                $mail->isSMTP();                                            // Send using SMTP
+                $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+                $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+                $mail->Username   = 'mongtamquoc2015@gmail.com';                     // SMTP username
+                $mail->Password   = 'Xuanhung123123';                               // SMTP password
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+                $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+                //Recipientsi
+                $mail->setFrom('mongtamquoc2015@gmail.com','Namisan');
+                $mail->addAddress($to , $name);
+                $mail->addReplyTo($to);
+
+                // Content
+                $mail->isHTML(true);                                  // Set email format to HTML
+                $mail->Subject = "E-Shop";
+                $mail->Body    =  "Xin chào " .$name . ",bạn đã đăng ký tài khoản thành công tại website E-Shop của chúng tôi,chúc bạn mua sắm vui vẻ!";
+
+                $mail->send();
+            } catch (Exception $e) {
+                echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            }
+        }
+
+    }
+
+
+
 }
